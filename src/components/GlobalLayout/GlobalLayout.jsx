e7abd28c0bd114d9017420f9871c23cd152b0ae5
Zyabrik10 <sasamazurok@gmail.com>
2023-07-28 12:07:50 +0300

    links

--- src/components/GlobalLayout/GlobalLayout.jsx
+++ src/components/GlobalLayout/GlobalLayout.jsx
@@ -18,12 +18,18 @@ export default function GlobalLayout() {
           <nav>
             <ul className="nav-links-list global-list">
               <li>
-                <StyledLink className="nav-link global-link" to="/">
+                <StyledLink
+                  className="nav-link global-link"
+                  to="/goit-react-hw-05-movie"
+                >
                   Home
                 </StyledLink>
               </li>
               <li>
-                <StyledLink className="nav-link global-link" to="/movies">
+                <StyledLink
+                  className="nav-link global-link"
+                  to="/goit-react-hw-05-movie/movies"
+                >
                   Movies
                 </StyledLink>
               </li>
